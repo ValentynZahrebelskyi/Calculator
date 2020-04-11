78c527b93d6b2248860f7ed6d422f748a2b85860
Valentine <ichchi256@gmail.com>
2020-04-11 09:55:21 +0300

    Initial Commit

---
+++ Calculator_landscape/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator_landscape
+//
+//  Created by Загребельский Валентин on 11.04.2020.
+//  Copyright © 2020 Загребельский Валентин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
